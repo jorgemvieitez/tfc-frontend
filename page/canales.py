460b12327cb38aacc4b19127b39fb8f97a136581
Jorge Moreno Vieitez <jorgemvieitez@gmail.com>
2025-05-31 16:59:50 +0200

    Resolver un par de errores

--- page/canales.py
+++ page/canales.py
@@ -26,8 +26,6 @@ def window(user, project):
     frm = ttk.Frame(root)
     frm.grid()
     ttk.Label(frm, text=f"Proyecto: {info_proj['nombre']}").grid(column=0, row=0, columnspan=4)
-    ttk.Label(frm, text=f"Canales").grid(column=0, row=1, columnspan=2)
-    ttk.Label(frm, text=f"Miembros").grid(column=2, row=1, columnspan=2)
     
     def boton_atras():
         root.destroy()
@@ -52,32 +50,37 @@ def window(user, project):
         proyectos.window(user)
     
     def boton_borrar_miembro(id):
-        def inner():
+        def inner(): pass
             
         return inner
 
-    row_c = 2
-    row_m = 2
+    frm_main = tkinter.Frame(frm)
+    frm_main.grid(columnspan=4)
+    ttk.Label(frm_main, text=f"Canales").grid(column=0, row=0, columnspan=2)
+    ttk.Label(frm_main, text=f"Miembros").grid(column=2, row=0, columnspan=2)
+
+    if len(canales) == 0:
+        ttk.Label(frm_main, text="<Ningún canal>").grid()
 
     for c in canales:
-        clr_frm = tkinter.Frame(frm,borderwidth=1, bg="grey")
+        clr_frm = tkinter.Frame(frm_main,borderwidth=1, bg="grey")
         clr_frm.grid(pady=5, padx=5, columnspan=2)
         dat_frm = tkinter.Frame(clr_frm)
         dat_frm.grid()
         ttk.Label(dat_frm, text=c["nombre"]).grid(column=0, row=0, padx=5, pady=5)
         ttk.Label(dat_frm, text=c["type"]).grid(column=0, row=1, padx=5, pady=(0, 5))
-        btn_abrir = ttk.Button(dat_frm, command=boton_abrir(c["id"]), text="Abrir")
+        btn_abrir = ttk.Button(dat_frm, text="Abrir")
         btn_abrir.grid(column=1, row=0, rowspan=2, pady=5, padx=(0, 5))
 
         btn_abrir["state"] = "disabled"
 
-        row_c += 1
+    row = 1
 
     for m in miembros:
         (status, m_usr) = api.rq("get", f"user/{m['usuario']}", user)
 
-        clr_frm = tkinter.Frame(frm,borderwidth=1, bg="grey")
-        clr_frm.grid(pady=5, padx=5, columnspan=2, column=2, row=row_m)
+        clr_frm = tkinter.Frame(frm_main,borderwidth=1, bg="grey")
+        clr_frm.grid(pady=5, padx=5, columnspan=2, column=2, row=row)
         dat_frm = tkinter.Frame(clr_frm)
         dat_frm.grid()
         ttk.Label(dat_frm, text=m_usr["nombre"]).grid(column=2, row=0, padx=5, pady=5)
@@ -88,15 +91,13 @@ def window(user, project):
         dat_frm_usr = tkinter.Frame(clr_frm_usr)
         dat_frm_usr.grid()
         tkinter.Button(dat_frm_usr, text="...").grid(column=0, row=0)
-        tkinter.Button(dat_frm_usr, text="X", command=boton_borrar_miembro).grid(column=1, row=0)
+        tkinter.Button(dat_frm_usr, text="X", command=boton_borrar_miembro(m_usr["id"])).grid(column=1, row=0)
 
-        row_m += 1
-    
-    row = max(row_c, row_m)
+        row += 1
 
-    ttk.Button(frm, text="Atrás", command=boton_atras).grid(column=0, row=row)
-    ttk.Button(frm, text="Nombre").grid(column=1, row=row)
-    ttk.Button(frm, text="+ Gente").grid(column=2, row=row)
-    ttk.Button(frm, text="Eliminar", style="Red.TButton", command=boton_borrar).grid(column=3, row=row)
+    ttk.Button(frm, text="Atrás", command=boton_atras).grid(column=0, row=3)
+    ttk.Button(frm, text="Nombre").grid(column=1, row=3)
+    ttk.Button(frm, text="+ Gente").grid(column=2, row=3)
+    ttk.Button(frm, text="Eliminar", style="Red.TButton", command=boton_borrar).grid(column=3, row=3)
     
     root.mainloop()
